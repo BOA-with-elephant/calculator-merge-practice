d0261298994860c7b911568f592b7725dcbc495f
DESKTOP-43RUNA1\USER <rhkdlf48301@gmail.com>
2025-05-28 11:46:49 +0900

    #4/feature_multiple

--- src/main/java/com/ohgiraffers/run/Application.java
+++ src/main/java/com/ohgiraffers/run/Application.java
@@ -1,7 +1,23 @@
 package com.ohgiraffers.run;
 
+import com.ohgiraffers.multiple.Multiple;
+
+import java.util.Scanner;
+
 public class Application {
+
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 숫자를 입력하세요 : ");
+        int firstNum = sc.nextInt();
+        System.out.print("두 번재 숫자를 입력하세요 : ");
+        int secondNum = sc.nextInt();
+
+        Multiple multiple = new Multiple();
+        multiple.calMultiple(firstNum, secondNum);
+
+
     }
+
 }
