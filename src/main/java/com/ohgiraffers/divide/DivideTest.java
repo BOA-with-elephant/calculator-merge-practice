c486cc4cb2fad9c1e891b9b8fcd8bde433618b06
21jayoon <21jayoon@gmail.com>
2025-05-28 11:46:38 +0900

    Completed feature: divide

---
+++ src/main/java/com/ohgiraffers/divide/DivideTest.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.divide;
+
+public class DivideTest {
+    public void divideTwoNum(int num1, int num2)
+            throws IllegalArgumentException {
+        if (num1 > 0 && num2 > 0) {
+            int divide = num1 / num2;
+            System.out.println(divide);
+        } else{
+            throw new IllegalArgumentException("0은 나눌 수 없습니다.");
+        }
+    }
+}
